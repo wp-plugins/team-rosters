704bbb52f3463dcafac9704a7f4f54e85f3fa5d5
MarkODonnell <MarkODonnell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-19 03:56:55 +0000

    New for 3.0 to support WP Color Selector
    git-svn-id: https://plugins.svn.wordpress.org/team-rosters/trunk@742997 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/tr-color-settings.js
@@ -0,0 +1,26 @@
+//Set up the color pickers to work with our text input field
+jQuery(document).ready(function($){
+	$('.tr_table_links_color').wpColorPicker();
+	
+	$('.tr_table_title_text_color').wpColorPicker();
+	
+	$('.tr_table_head_bkgd_color').wpColorPicker();
+	$('.tr_table_head_text_color').wpColorPicker();
+	
+    $('.tr_table_even_row_color').wpColorPicker();
+	$('.tr_table_even_row_bkgd').wpColorPicker();
+	
+	$('.tr_table_odd_row_bkgd').wpColorPicker();
+	$('.tr_table_odd_row_color').wpColorPicker();
+	
+	$('.sp_main_bkgd_color').wpColorPicker();
+	$('.sp_main_text_color').wpColorPicker();
+	
+	$('.sp_bio_border_color').wpColorPicker();
+	$('.sp_bio_header_color').wpColorPicker();
+	$('.sp_bio_bkgd_color').wpColorPicker();
+	$('.sp_bio_text_color').wpColorPicker();
+	
+	$('.gallery_links_color').wpColorPicker();
+	
+});
